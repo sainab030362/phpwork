77107da18caee8e463865ed0c6cb26ba51ff7fda
sainab030362 <sainab030362@gmail.com>
2022-10-21 11:48:48 +0700

    ส่งไฟล์Comment

---
+++ comment.php
@@ -0,0 +1,15 @@
+<?php
+
+//บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+echo "Saianb Yaring";
+echo "Information Technology";
+//echo "Pattani<br>";
+#echo "Thailand<br>";
+
+//บรรทัดที่ 10-12 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+/*echo "<br>";
+echo "94000<br>";
+echo "Asia<br>";*/
+echo "Univerasal";
+
+?>
